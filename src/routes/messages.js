e8123d6f678dac23cc64ffaf49384679e16f3dc8
Rohit Acharya <rohitacharya051005@gmail.com>
2025-08-05 22:58:45 +0530

    change profile img

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -4,7 +4,13 @@ import { sendmail } from '../api/message.js';
 
 
 const router= express.Router();
-
+router.options('/sendmail', (req, res) => {
+    // This is the preflight request, send back the necessary CORS headers
+    res.header('Access-Control-Allow-Origin', 'https://portfolio-seven-inky-37.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add other headers if you use them
+    res.status(200).send();
+});
 router.post("/sendmail",sendmail)
 
 export default router;
